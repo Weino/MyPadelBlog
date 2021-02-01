3c7fb53b6fec4f8b2b06748acb681575b205e813
William Weinö <William.Weino@hotmail.com>
2021-02-01 19:21:55 +0100

    New comments + small css change

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+/* Jag använder mig av en konstruktor för att kunna ha en "blueprint" för att skapa många objekt av samma typ, detta underlättar när man ska skapa många saker av en typ, då är
+det bara att lägga till en 'player', som har en viss "regel" i sig som säger att den måste ha ett "namn, efternamn, en bild och en ålder". I JS är "this" inte en variable utan
+ett "keyword". https://www.youtube.com/watch?v=Mus_vwhTCq0 är en video som förklarde bra hur man skriver sin kod smart och lätt, därifrån fick jag några ideér såsom min "player" 
+function*/
+
 window.addEventListener('load', () => {
 })
 
@@ -31,11 +36,20 @@ function onNumberOfPlayerChange(inputBox){
 
 function getPlayers(numberOfPlayers){
 
-    let player1 = new Player('Juan','Lebron','https://padelworldpress.es/wp-content/uploads/2020/08/Juan-Lbron.jpg', 25);
-    let player2 = new Player('Alejandro','Galn Romo','https://www.worldpadeltour.com/media-content/2020/09/destacada-alejandro-galn-romo-f38f1626f6-1200x500.JPG', 23);
-    let player3 = new Player('Francisco','Navarro Compán','https://www.worldpadeltour.com/media-content/2020/03/destacada-francisco-navarro-compn-71b9866456-1200x500.JPG', 41 );
+    let player1 = new Player('1. Juan','Lebron','https://padelworldpress.es/wp-content/uploads/2020/08/Juan-Lbron.jpg', 25);
+    let player2 = new Player('Alejandro','Galán Romo','https://www.worldpadeltour.com/media-content/2020/09/destacada-alejandro-galn-romo-f38f1626f6-1200x500.JPG', 23);
+    let player3 = new Player('Francisco','Navarro Compán','https://www.worldpadeltour.com/media-content/2020/03/destacada-francisco-navarro-compn-71b9866456-1200x500.JPG', 31 );
+    let player4 = new Player('Carlos', 'Gutiérrez', 'https://www.worldpadeltour.com/media-content/2020/03/destacada-carlos-daniel-gutirrez-d868df0ce0-1200x500.JPG', 36)
+    let player5 = new Player('Pablo' , 'Lima', 'https://www.worldpadeltour.com/media-content/2019/12/lima-mvp-master-final-774x435.jpg', 34)
+    let player6 = new Player('Fernando', 'Belasteguín' ,'https://www.worldpadeltour.com/media-content/2020/09/destacada-fernando-belastegun-c444417350-1200x500.JPG', 41)
+    let player7 = new Player('Agustín','Tapia' ,'https://lh3.googleusercontent.com/-xgQwJnLY-1M/XxBqn8DD3XI/AAAAAAAAdCU/Akf_J0pi2l0UX9dAefofXRRIdlnBPcrowCLcBGAsYHQ/s1600/IMG-20200716-WA0023.jpg', 21)
+    let player8 = new Player('Maximiliano', 'Sánchez', 'https://www.worldpadeltour.com/media-content/2016/08/Maxi-Sanchez-2-768x432.jpg', 34)
+    let player9 = new Player('Franco' , 'Stupaczuk', 'https://www.worldpadeltour.com/media-content/2019/05/franco-stupaczuk-cuartos-vigo-open--774x435.jpg', 24)
+    let player10 = new Player('Federico','Chingotto' , 'https://padelmagazine.fr/wp-content/uploads/2020/01/juan-tello-bandeja.jpg' , 23)
 
-    let allPlayers = [player1, player2, player3];
+
+
+    let allPlayers = [player1, player2, player3, player4, player5, player6, player7, player8, player9, player10];
 
     return allPlayers.slice(0,numberOfPlayers);
 }
